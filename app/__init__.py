8ecc805413f8d28e2f3a1d3a0b62c6b9aeae2205
waimea-cmmarshwetere <cmmarshwetere@waimea.school.nz>
2025-06-17 13:25:29 +1200

    Tasks show up YIPPEEEE!!!

--- app/__init__.py
+++ app/__init__.py
@@ -25,45 +25,15 @@ register_error_handlers(app)
 #-----------------------------------------------------------
 @app.get("/")
 def index():
-
-    return render_template("pages/home.jinja")
-
-
-#-----------------------------------------------------------
-# Things page route - Show all the things, and new thing form
-#-----------------------------------------------------------
-@app.get("/things/")
-def show_all_things():
     with connect_db() as client:
         # Get all the things from the DB
-        sql = "SELECT id, name FROM things ORDER BY name ASC"
+        sql = "SELECT id, name, priority, complete FROM tasks ORDER BY priority DESC"
         result = client.execute(sql)
-        things = result.rows
+        tasks = result.rows
 
         # And show them on the page
-        return render_template("pages/things.jinja", things=things)
 
-
-#-----------------------------------------------------------
-# Thing page route - Show details of a single thing
-#-----------------------------------------------------------
-@app.get("/thing/<int:id>")
-def show_one_thing(id):
-    with connect_db() as client:
-        # Get the thing details from the DB
-        sql = "SELECT id, name, price FROM things WHERE id=?"
-        values = [id]
-        result = client.execute(sql, values)
-
-        # Did we get a result?
-        if result.rows:
-            # yes, so show it on the page
-            thing = result.rows[0]
-            return render_template("pages/thing.jinja", thing=thing)
-
-        else:
-            # No, so show error
-            return not_found_error()
+    return render_template("pages/home.jinja", tasks=tasks)
 
 
 #-----------------------------------------------------------
@@ -72,21 +42,21 @@ def show_one_thing(id):
 @app.post("/add")
 def add_a_thing():
     # Get the data from the form
-    name  = request.form.get("name")
+    task  = request.form.get("task")
     priority = request.form.get("priority")
 
     # Sanitise the inputs
-    name = html.escape(name)
+    task = html.escape(task)
     priority = html.escape(priority)
 
     with connect_db() as client:
         # Add the thing to the DB
         sql = "INSERT INTO things (name, priority) VALUES (?, ?)"
-        values = [name, priority]
+        values = [task, priority]
         client.execute(sql, values)
 
         # Go back to the home page
-        flash(f"Thing '{name}' added", "success")
+        flash(f"Thing '{task}' added", "success")
         return redirect("/")
 
 
